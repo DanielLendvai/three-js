ed4ccaf5ff6975cd7848340d104faf71a9c94edc
Daniel Lendvai <lendvai.daniel92@gmail.com>
2022-11-01 13:58:32 +0100

    practice

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 import * as THREE from "./node_modules/three/build/three.module.js";
+import { RGBELoader } from "./node_modules/three/examples/jsm/loaders/RGBELoader.js";
 import { OrbitControls } from "./node_modules/three/examples/jsm/controls/OrbitControls.js";
 import gsap from "./node_modules/gsap/index.js";
 
 const body = document.querySelector("body");
-const about = document.querySelector(".about-me");
-const hello = document.querySelector("h1:nth-child(2)");
-const hello2 = document.querySelector("body > h1:nth-child(3)");
-const button = document.querySelector(".btn-action-first");
-const buttons = document.querySelector(".buttons");
-const cubeTexture = new THREE.TextureLoader().load("textures/neon.jpg");
+body.insertAdjacentHTML("beforeend", `<h1>Welcome</h1>`);
+const hello = document.querySelector("h1");
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -16,49 +13,46 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 );
-
-const renderer = new THREE.WebGLRenderer({ body, alpha: true });
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setClearColor(0x000000);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 0.6;
+renderer.outputEncoding = THREE.sRGBEncoding;
 document.body.appendChild(renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1.5, 1.5, 1.5);
+const geometry = new THREE.SphereGeometry(1.5, 30, 30);
 const material = new THREE.MeshPhongMaterial({
-    color: 0xffffff,
-    map: cubeTexture,
+    map: new THREE.TextureLoader().load("textures/earthmap1k.jpg"),
+    bumpMap: new THREE.TextureLoader().load("textures/earthbump.jpg"),
+    bumpScale: 0.3,
 });
-// const material = new THREE.MeshPhongMaterial({ color: 0xffffff, map: texture });
-const cube = new THREE.Mesh(geometry, material);
+const globe = new THREE.Mesh(geometry, material);
 
-let alight = new THREE.AmbientLight(0xffffff, 1);
-scene.add(alight);
-let plight = new THREE.PointLight(0xffffff, 4);
-let plight2 = new THREE.PointLight(0xffffff, 4);
-plight.position.set(0, 5, 0);
-plight2.position.set(5, -5, 5);
-plight2.lookAt(0, 0, 0);
-plight.lookAt(0, 0, 0);
-scene.add(plight);
-scene.add(plight2);
+const ambientlight = new THREE.AmbientLight(0xffffff, 0.2);
+scene.add(ambientlight);
 
-setTimeout(() => {
-    hello.classList.toggle("hidden");
-    hello2.classList.toggle("hidden");
-}, 2000);
-scene.add(cube);
-cube.position.y = 10;
-setTimeout(() => {
-    gsap.to(cube.position, { x: 0, y: 0, z: 0, duration: 2 });
-    gsap.to(cube.rotation, {
-        y: 5,
-        z: 5,
-        x: 5,
-        duration: 2,
-    });
-    buttons.classList.toggle("hidden");
-    hello2.classList.toggle("hidden");
-}, 4000);
+// set point light
+
+const pointerlight = new THREE.PointLight(0xffffff, 0.5);
+
+// set light position
+
+pointerlight.position.set(5, 3, 5);
+scene.add(pointerlight);
+
+const stargeometry = new THREE.SphereGeometry(20, 20, 20);
+
+const starmaterial = new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load("textures/galaxy.png"),
+    side: THREE.BackSide,
+});
+
+const starmesh = new THREE.Mesh(stargeometry, starmaterial);
+
+scene.add(starmesh);
 
 camera.position.z = 10;
 camera.position.x = 0;
@@ -66,37 +60,23 @@ let speed = 0.01;
 
 function animate() {
     requestAnimationFrame(animate);
+
+    controls.update();
     renderer.render(scene, camera);
-    cube.rotateY(Math.sin(speed));
-    cube.rotateX(Math.sin(speed));
-    if (cube.position.x != 0) {
-        button.innerHTML = "Vissza";
-    } else {
-        button.innerHTML = "Tovább";
-    }
+    globe.rotateY(Math.sin(speed));
+    globe.rotateX(Math.sin(speed));
 }
-
-button.addEventListener("click", () => {
-    // gsap.to(cube.position, {x: 10, duration: 5});
-    // button.classList.toggle("float-left-top");
-    about.classList.toggle("visible");
-
-    if (cube.position.x === 0) {
-        gsap.to(cube.position, { y: 5, x: -13, duration: 3 });
-        gsap.to(cube.rotation, {
-          y: 5,
-          z: 5,
-          x: 5,
-          duration: 2,
-      });
-    } else {
-        gsap.to(cube.position, { y: 0, x: 0, duration: 2 });
-    }
-});
 
 animate();
 window.addEventListener("resize", onWindowResize, false);
+scene.add(globe);
 
+/* new RGBELoader().load("textures/sky4k.hdr", function (texture) {
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+    scene.background = texture;
+    scene.environment = texture;
+});
+ */
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
